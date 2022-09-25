5ffe0af90615934a5cdb9c58ea53980f960be95a
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2022-09-24 19:15:31 -0500

    Creo el main XD

---
+++ src/application/App.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/FrameInicial.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+
+}
